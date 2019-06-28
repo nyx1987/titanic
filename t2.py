7a886744a61c894b71d06518012e138d27e648a4
308160659@qq.com <root@huyaoweideMacBook-Pro.local>
2019-06-28 17:08:54 +0800

    test

---
+++ t2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+train=pd.read_csv("/Users/wwwroot/python/titanic/data/train.csv")
+test=pd.read_csv("/Users/wwwroot/python/titanic/data/test.csv")
+PassengerId=test['PassengerId']
+#print(test.info())
+#print(train.describe())
+print(train.head(8))
